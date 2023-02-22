712a0ec5f913b4b0977614d583617f03c29084bc
Islay Anderson <mranderson64@live.com>
2023-02-22 01:55:04 +0000

    quantity prevent default

--- scripts/main.js
+++ scripts/main.js
@@ -10,10 +10,12 @@ function quantity(){
     let quantity = document.querySelectorAll('#quantity');
     let ui = document.querySelectorAll('quantity_ui button');
     if(quantity){
-        ui[0].addEventListener('click', function(){
+        ui[0].addEventListener('click', function(e){
+            e.preventDefault();
             quantity.value = quantity.value++;
         })
-        ui[1].addEventListener('click', function(){
+        ui[1].addEventListener('click', function(e){
+            e.preventDefault();
             quantity.value = quantity.value--;
         })
     }
