8f1f6a0e9df350787baba24e5700936be2b45d07
Islay Anderson <mranderson64@live.com>
2023-02-22 02:48:55 +0000

    missing qualifier

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ window.onload = function(){
 //event listeners 
 function quantity(){
     let quantity = document.querySelectorAll('#quantity');
-    let ui = document.querySelectorAll('quantity_ui button');
+    let ui = document.querySelectorAll('.quantity_ui button');
     if(quantity){
         ui[0].addEventListener('click', function(e){
             //e.preventDefault();
