3ca66a0aab52077ca0432fa90f1f0e431084112e
Islay Anderson <mranderson64@live.com>
2023-02-22 03:28:57 +0000

    debug

--- scripts/main.js
+++ scripts/main.js
@@ -13,12 +13,12 @@ function quantity(){
     if(quantity){
         ui[0].addEventListener('click', function(e){
             //e.preventDefault();
-            quantity.value = quantity.value++;
+            quantity.value++;
             console.log(quantity.value)
         })
         ui[1].addEventListener('click', function(e){
             //e.preventDefault();
-            quantity.value = quantity.value--;
+            quantity.value--;
             console.log(quantity.value)
         })
     }
