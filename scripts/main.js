4edeb3cfdaf6032d4bf777e6544e3e87190ba026
Islay Anderson <mranderson64@live.com>
2023-02-22 03:21:37 +0000

    debug

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ window.onload = function(){
 function quantity(){
     let quantity = document.querySelectorAll('#quantity');
     let ui = document.querySelectorAll('.quantity_ui button');
+    console.log(ui)
     if(quantity){
         ui[0].addEventListener('click', function(e){
             //e.preventDefault();
